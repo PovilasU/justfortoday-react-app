2a02c5dd891775e1fb1c250c3392e28dcad76423
PovilasU <povilas.urbonas85@gmail.com>
2025-05-29 12:32:37 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+// import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p className="read-the-docs">React App with Vite test123</p>
+    </>
+  );
+}
+
+export default App;
